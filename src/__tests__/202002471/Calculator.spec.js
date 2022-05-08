9536866d57852d0e33b3e2f9ca91e7406d912451
LONGNEW <shinhyoung26@gmail.com>
2022-05-08 20:55:11 +0900

    modify: toBeTruthy() call function

--- src/__tests__/202002471/Calculator.spec.js
+++ src/__tests__/202002471/Calculator.spec.js
@@ -19,13 +19,13 @@ afterEach(() => {
 const cal = new Calculator();
 describe("음수 양수 판단", () => {
   test("isNegative에 7을 넣으면 false를 반환한다.", () => {
-    expect(cal.isNegative(7)).toBeFalsy;
+    expect(cal.isNegative(7)).toBeFalsy();
   });
   test("isNegative에 -3을 넣으면 true를 반환한다.", () => {
-    expect(cal.isNegative(-3)).toBeTruthy;
+    expect(cal.isNegative(-3)).toBeTruthy();
   });
   test("isNegative에 0을 넣으면 true가 아니다.", () => {
-    expect(cal.isNegative(0)).not.toBeTruthy;
+    expect(cal.isNegative(0)).not.toBeTruthy();
   });
 });
 
@@ -45,24 +45,22 @@ describe("abs에 대한 테스트 코드", () => {
 });
 
 describe("가상 함수 테스트해보기", () => {
-
-    test("customCalculation는 전달 된 함수를 호출시킨다.", () => {
-      const cal = new Calculator();
-      const mockCallback = jest.fn();
-      cal.customCalculation(mockCallback, ["a", "b"]);
-      expect(mockCallback).toHaveBeenCalled();
-    });
-    test("customCalculation에 함수와 1, 2, 3인자를 전달하면, 전달 된 함수가 인자 1, 2, 3을 받아 호출한다.", () => {
-      const cal = new Calculator();
-      const mockCallback = jest.fn();
-      cal.customCalculation(mockCallback, [1, 2, 3]);
-      expect(mockCallback).toHaveBeenCalledWith([1, 2, 3]);
-    });
-    test("abs함수는 Math.abs를 호출한다.", () => {
-      const cal = new Calculator();
-      const spy = jest.spyOn(Math, 'abs');
-      cal.abs(0);
-      expect(spy).toBeCalledTimes(1);
-    });
-
+  test("customCalculation는 전달 된 함수를 호출시킨다.", () => {
+    const cal = new Calculator();
+    const mockCallback = jest.fn();
+    cal.customCalculation(mockCallback, ["a", "b"]);
+    expect(mockCallback).toHaveBeenCalled();
+  });
+  test("customCalculation에 함수와 1, 2, 3인자를 전달하면, 전달 된 함수가 인자 1, 2, 3을 받아 호출한다.", () => {
+    const cal = new Calculator();
+    const mockCallback = jest.fn();
+    cal.customCalculation(mockCallback, [1, 2, 3]);
+    expect(mockCallback).toHaveBeenCalledWith([1, 2, 3]);
+  });
+  test("abs함수는 Math.abs를 호출한다.", () => {
+    const cal = new Calculator();
+    const spy = jest.spyOn(Math, "abs");
+    cal.abs(0);
+    expect(spy).toBeCalledTimes(1);
+  });
 });
