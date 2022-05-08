e96f6d26775c35470042131bbeeaa613e9d057af
HyunSoo Kim <39149858+dblepart99@users.noreply.github.com>
2022-05-08 20:53:57 +0900

    modify: toBeTruthy() call function

--- src/__tests__/201802081/Calculator.spec.js
+++ src/__tests__/201802081/Calculator.spec.js
@@ -44,18 +44,16 @@ describe("Date 클래스 판단하기", () => {
 });
 
 describe("음수 양수 판단", () => {
-  test("isNegative에 7을 넣으면 false를 반환한다.", () => {
     const cal = new Calculator();
-    expect(cal.isNegative(7)).toBeFalsy;
-  });
-  test("isNegative에 -3을 넣으면 true를 반환한다.", () => {
-    const cal = new Calculator();
-    expect(cal.isNegative(-3)).toBeTruthy;
-  });
-  test("isNegative에 0을 넣으면 true가 아니다.", () => {
-    const cal = new Calculator();
-    expect(cal.isNegative(7)).toBeTruthy;
-  });
+    test("isNegative에 7을 넣으면 false를 반환한다.", () => {
+      expect(cal.isNegative(7)).toBeFalsy();
+    });
+    test("isNegative에 -3을 넣으면 true를 반환한다.", () => {
+        expect(cal.isNegative(-3)).toBeTruthy();
+    });
+    test("isNegative에 0을 넣으면 true가 아니다.", () => {
+        expect(cal.isNegative(0)).not.toBeTruthy();
+    });
 });
 
 describe("abs에 대한 테스트 코드", () => {
